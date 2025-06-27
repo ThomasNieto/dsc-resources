d7a0aa769d82d6dfe39651962e42622503337089
Thomas Nieto <38873752+ThomasNieto@users.noreply.github.com>
2025-06-26 23:21:53 -0500

    Remove File

--- src/File/FileConfig.cs
+++
@@ -1,8 +0,0 @@
-using System.Text;
-
-public class FileConfig(string path, string contents, Encoding encoding)
-{
-    public string Path { get; } = path;
-    public string Contents { get; } = contents;
-    public Encoding Encoding { get; } = encoding;
-}
